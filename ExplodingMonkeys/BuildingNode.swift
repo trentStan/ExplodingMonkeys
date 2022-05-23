ffe70eeaff337699e122021e7bc9ab2517daeada
trentStan <71011863+trentStan@users.noreply.github.com>
2022-05-23 11:13:33 +0200

    completed the game

--- ExplodingMonkeys/BuildingNode.swift
+++ ExplodingMonkeys/BuildingNode.swift
@@ -12,23 +12,23 @@ import UIKit
 
 class BuildingNode: SKSpriteNode {
     var currentImage: UIImage!
-
+    
     func setup() {
         name = "building"
-
+        
         currentImage = drawBuilding(size: size)
         texture = SKTexture(image: currentImage)
-
+        
         configurePhysics()
     }
-
+    
     func configurePhysics() {
         physicsBody = SKPhysicsBody(texture: texture!, size: size)
         physicsBody!.isDynamic = false
         physicsBody!.categoryBitMask = CollisionTypes.building.rawValue
         physicsBody!.contactTestBitMask = CollisionTypes.banana.rawValue
     }
-
+    
     func drawBuilding(size: CGSize) -> UIImage {
         // 1
         let renderer = UIGraphicsImageRenderer(size: size)
@@ -36,7 +36,7 @@ class BuildingNode: SKSpriteNode {
             // 2
             let rectangle = CGRect(x: 0, y: 0, width: size.width, height: size.height)
             var color: UIColor
-
+            
             switch Int.random(in: 0...2) {
             case 0:
                 color = UIColor(hue: 0.502, saturation: 0.98, brightness: 0.67, alpha: 1)
@@ -45,15 +45,15 @@ class BuildingNode: SKSpriteNode {
             default:
                 color = UIColor(hue: 0, saturation: 0, brightness: 0.67, alpha: 1)
             }
-
+            
             ctx.cgContext.setFillColor(color.cgColor)
             ctx.cgContext.addRect(rectangle)
             ctx.cgContext.drawPath(using: .fill)
-
+            
             // 3
             let lightOnColor = UIColor(hue: 0.190, saturation: 0.67, brightness: 0.99, alpha: 1)
             let lightOffColor = UIColor(hue: 0, saturation: 0, brightness: 0.34, alpha: 1)
-
+            
             for row in stride(from: 10, to: Int(size.height - 10), by: 40) {
                 for col in stride(from: 10, to: Int(size.width - 10), by: 40) {
                     if Bool.random() {
@@ -61,16 +61,35 @@ class BuildingNode: SKSpriteNode {
                     } else {
                         ctx.cgContext.setFillColor(lightOffColor.cgColor)
                     }
-
+                    
                     ctx.cgContext.fill(CGRect(x: col, y: row, width: 15, height: 20))
                 }
             }
-
+            
             // 4
         }
-
+        
         return img
     }
-
-   
+    
+    func hit(at point: CGPoint) {
+        let convertedPoint = CGPoint(x: point.x + size.width / 2, y: abs(point.y - (size.height / 2)))
+        
+        let renderer = UIGraphicsImageRenderer(size: size)
+        
+        
+        let img = renderer.image { ctx in
+            
+            currentImage.draw(at: .zero)
+            ctx.cgContext.addEllipse(in: CGRect(x: convertedPoint.x - 32, y: convertedPoint.y - 32, width: 64, height: 64))
+            ctx.cgContext.setBlendMode(.clear)
+            ctx.cgContext.drawPath(using: .fill)
+        
+        }
+        
+        texture = SKTexture(image: img)
+                   currentImage = img
+                   configurePhysics()
+    }
 }
+
